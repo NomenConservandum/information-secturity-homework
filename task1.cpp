a8bfc84f1b6903d573ca4688157ab727cd33f0e8
Nomen Conservandum <hramov.sanya2018@yandex.ru>
2026-09-22 20:09:44 +0400

    Task 1: refactoring (code); add (simple hash-file check)

--- task1.cpp
+++ task1.cpp
@@ -6,14 +6,31 @@
 
 using namespace std;
 
-// for DFS you might need a stack of strings (simply paths)
+// TODO: write two functions (read and write hash-file functions)
 
 // look at 3 cases: a file is added; a file is deleted; a file is modified (the hash is different)
 int main(int argc, char* argv[]) {
     filesystem::path dir = argv[1];
     
-    if (!filesystem::exists(dir)) { // and check for hash-file
+    if (!filesystem::exists(dir)) { // check the directory given
         cout << "No such directory!" << endl;
+        return 1;
+    }
+    
+    filesystem::path hashFileDir = dir / "hash-file.txt";
+
+    if (!filesystem::exists(hashFileDir)) { // check for hash-file
+        // create one
+        ofstream hashFile(hashFileDir);
+        hashFile.close();
+    }
+
+    fstream hashFile(hashFileDir); // I have two ways:
+    // 1. put all of the hash-file work into functions;
+    // 2. read it here once into a map and then write new contents into the file
+
+    if (!hashFile.is_open()) {
+        cout << "Error opening the hash-file!" << endl;
         return 1;
     }
 
@@ -26,11 +43,10 @@ int main(int argc, char* argv[]) {
 
         for ( const filesystem::directory_entry& entry :
               filesystem::directory_iterator(path) ) {
-            if (entry.is_directory() || !entry.is_regular_file()) {
+            if (entry.is_directory())
                 DFSStack.push(entry.path());
-                //cout << "[DIR]: " << entry.path() << endl;
+            if (entry.is_directory() || entry.path() == dir / "hash-file.txt")
                 continue;
-            }
 
             // open the file and do the hash
             ifstream file(entry.path());
@@ -49,10 +65,14 @@ int main(int argc, char* argv[]) {
 
             hash<string> hash;
 
+            // store in a map where key is path and value is hash
+
             // the newline character is the best delimeter I could think of
-            cout << entry.path() << "\n" << hash(content) << endl; 
+            cout << entry.path() << "\n" << hash(content) << endl;
         }
     }
+
+    hashFile.close();
 
     return 0;
 }
