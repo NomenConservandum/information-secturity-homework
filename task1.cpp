571104221e0655f0d52106e4b0b43defd9a19eaf
Nomen Conservandum <hramov.sanya2018@yandex.ru>
2026-09-22 19:34:24 +0400

    Task 1: implement (hash)

--- task1.cpp
+++ task1.cpp
@@ -1,7 +1,8 @@
 #include <iostream>
 #include <filesystem>
 #include <stack>
-
+#include <fstream>
+#include <string>
 
 using namespace std;
 
@@ -13,7 +14,7 @@ int main(int argc, char* argv[]) {
     
     if (!filesystem::exists(dir)) { // and check for hash-file
         cout << "No such directory!" << endl;
-        return 0;
+        return 1;
     }
 
     // DFS part
@@ -24,11 +25,30 @@ int main(int argc, char* argv[]) {
         DFSStack.pop();
 
         for (const filesystem::directory_entry& entry : filesystem::directory_iterator(path)) {
-            if (entry.is_regular_file())
-                cout << "[FILE]: ";
-            else
+            if (entry.is_regular_file()) { // do the hash
+                cout << "[FILE: HASH AND PATH]: ";
+                ifstream file(entry.path());
+
+                if (!file.is_open()) { // Will this even work on a non-text file?
+                    cout << "Error reading the file!" << endl;
+                    return 1;
+                }
+                
+                string content, temp;
+
+                while (getline(file, temp))
+                    content += temp;
+
+                file.close();
+
+                hash<string> hash;
+
+                cout << hash(content) << " | ";
+            } else
                 cout << "[DIR]: ";
+            
             cout << entry.path() << endl;
+
             if (entry.is_directory())
                 DFSStack.push(entry.path());
         }
