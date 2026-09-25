eee0bcea0364b48e79a6303e4cdd0a9aa3833877
Nomen Conservandum <hramov.sanya2018@yandex.ru>
2026-09-25 22:46:54 +0400

    Task 1: implement (map comparison, hash-file generation)

--- task1.cpp
+++ task1.cpp
@@ -9,12 +9,6 @@
 using namespace std;
 
 map<string, size_t>* readHashFile(filesystem::path dir) {
-    if (!filesystem::exists(dir)) { // check for hash-file
-        // create one
-        ofstream hashFile(dir);
-        hashFile.close();
-    }
-
     fstream hashFile(dir);
 
     if (!hashFile.is_open()) {
@@ -28,9 +22,8 @@ map<string, size_t>* readHashFile(filesystem::path dir) {
     string temp;
 
     while (getline(hashFile, temp)) {
-        string
-            to_string = temp.substr(0, temp.find_first_of(':')),
-            to_size_t = temp.substr(temp.find_first_of(':') + 1, temp.length() - temp.find_first_of(':'));
+        string to_string = temp.substr(1, temp.find_first_of(':') - 2),
+               to_size_t = temp.substr(temp.find_first_of(':') + 1, temp.length() - temp.find_first_of(':'));
         
         // convert the string to size_t this weird way
         stringstream stream(to_size_t);
@@ -57,12 +50,22 @@ int main(int argc, char* argv[]) {
     }
     
     filesystem::path hashFileDir = dir / "hash-file.txt";
+    
+    map<string, size_t> *oldHashes = nullptr, // read the hash-file
+                        *newHashes = new map<string, size_t>();
 
-    map<string, size_t>* hashes = readHashFile(hashFileDir); // read the hash-file
+    bool doesHashFileExist = filesystem::exists(hashFileDir);
+    if (doesHashFileExist)
+        oldHashes = readHashFile(hashFileDir);
+    else {
+        ofstream hashFile(hashFileDir);
+        hashFile.close();
+    }
 
     // DFS part
     stack<filesystem::path> DFSStack;
     DFSStack.push(dir);
+    ofstream hashFile(hashFileDir);
     while(!DFSStack.empty()) {
         filesystem::path path = DFSStack.top();
         DFSStack.pop();
@@ -77,8 +80,8 @@ int main(int argc, char* argv[]) {
             // open the file and do the hash
             ifstream file(entry.path());
 
-            if (!file.is_open()) { // Will this even work on a non-text file?
-                cout << "Error reading the file!" << endl;
+            if (!file.is_open()) { // Will this even work on a non-text file? UPD: seems so
+                cerr << "Error reading the file!" << endl;
                 return 1;
             }
             
@@ -94,11 +97,31 @@ int main(int argc, char* argv[]) {
             // store in a map where key is path and value is hash
 
             // the newline character is the best delimeter I could think of
-            cout << entry.path() << ":" << hash(content) << endl;
+            hashFile << entry.path() << ":" << hash(content) << endl;
+            // cerr << entry.path() << ":" << hash(content) << endl;
+            newHashes->insert({entry.path(), hash(content)});
         }
     }
+    hashFile.close();
 
-    // Give the user a choice: save the new hash file, if changes present, or keep the old one
+    if (oldHashes == nullptr)
+        return 0;
+    // A simple check. I'm too lazy to come up with something special.
+    for (const auto& it : *oldHashes) {
+        //cerr << "Looking for: " << it.first << endl;
+        if (newHashes->find(it.first) == newHashes->end()) { // no such element
+            cerr << "WARNING! THE FILE " << it.first << " HAS BEEN DELETED!" << endl;
+        } else if (it.second != newHashes->find(it.first)->second) { // The hashes do not align
+            cerr << "WARNING! THE FILE " << it.first << " HAS BEEN MODIFIED!" << endl;
+        }
+    }
+    for (const auto& it : *newHashes) {
+        //cerr << "Looking for: " << it.first << endl;
+        if (oldHashes->find(it.first) == oldHashes->end()) { // this is a new file
+            cerr << "WARNING! A NEW FILE HAS BEEN DETECTED: " << it.first << "!" << endl;
+        }
+    }
+    // Give the user a choice maybe: save the new hash file, if changes present, or keep the old one
 
     return 0;
 }
